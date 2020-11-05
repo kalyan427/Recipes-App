a4c0a8ff7c397b19554e7a75ae288b8a8d5d9b88
kalyan <pvkalyan427@gmail.com>
2020-11-05 07:42:41 -0800

    Need to add timer logic.

--- Recipes-App/Recipes-App/Timer/TimerViewController.swift
+++ Recipes-App/Recipes-App/Timer/TimerViewController.swift
@@ -10,28 +10,34 @@ import UIKit
 import HGCircularSlider
 
 class TimerViewController: UIViewController {
+    @IBOutlet weak var hourView: CircularSlider!
+    @IBOutlet weak var minuteView: CircularSlider!
     
-    var myFrame: CGRect = CGRect(x: 120, y: 120, width: 150, height: 150)
-    var primaryFrame: CGRect = CGRect(x: 90, y: 90, width: 220, height: 220)
+    
+//    var hourFrame: CGRect = CGRect(x: 120, y: 120, width: 150, height: 150)
+//    var minuteFrame: CGRect = CGRect(x: 90, y: 90, width: 220, height: 220)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Circular slider implementation.
-        let circularSlider = CircularSlider(frame: myFrame)
-        circularSlider.minimumValue = 0
-        circularSlider.maximumValue = 12
-        circularSlider.endPointValue = 6
-        circularSlider.backgroundColor = UIColor.clear
-        self.view.addSubview(circularSlider)
         
-        let secondCircularView = CircularSlider(frame: primaryFrame)
-        secondCircularView.minimumValue = 0
-        secondCircularView.maximumValue = 60
-        secondCircularView.endPointValue = 35
-        secondCircularView.diskColor = UIColor.clear
-        secondCircularView.backgroundColor = UIColor.clear
-        self.view.addSubview(secondCircularView)
+        
+        
+        
+        hourView.minimumValue = 0
+        hourView.maximumValue = 12
+        hourView.endPointValue = 6
+        hourView.backgroundColor = UIColor.clear
+    
+        
+        
+        minuteView.minimumValue = 0
+        minuteView.maximumValue = 60
+        minuteView.endPointValue = 35
+        minuteView.diskColor = UIColor.clear
+        minuteView.backgroundColor = UIColor.clear
+      
         
         setupSliders()
     }
